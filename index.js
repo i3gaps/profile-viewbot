0c23508600e5c7d2c2c24c326d5578d574b83b76
oxy <91498221+i3gaps@users.noreply.github.com>
2021-09-28 13:24:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const p = require('puppeteer')
+const yo = require('puppeteer')
 const chalk = require('chalk')
 const red = chalk.hex('#DC143C');
 
-const banner = (`
+console.clear()
+const ban =(`
 ${chalk.hex('#FFFFFF')("                               ┬  ┬┬┌─┐┬ ┬")}
 ${chalk.hex('#8D8C8C')("                               └┐┌┘│├┤ │││")}
 ${chalk.hex('#DC143C')("                                └┘ ┴└─┘└┴┘")}
 `)
 
-console.log(banner)
+console.log(ban)
 
 async function heyy() {
     console.log((`${red(`Sent a view.`)}`))
-    const browser = await p.launch();
+    const browser = await yo.launch();
     const page = await browser.newPage(); //opens page
-    await page.goto("Link")
+    await page.goto("https://camo.githubusercontent.com/0611cf09a7206ca10457a2958e022e3bfe133ba2424edafdb068a30e20556bea/68747470733a2f2f677076632e6172747572696f2e6465762f693367617073")
     await browser.close();
 }
 
-setInterval(heyy, 100)
+setInterval(heyy, 100) // don't set it below 100 unless you want your pc to crash
