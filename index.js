6a6f05f7e77a47d186e68d6630921f557e07f0ab
oxy <91498221+i3gaps@users.noreply.github.com>
2021-09-27 20:36:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const yo = require('puppeteer')
+const p = require('puppeteer')
 const chalk = require('chalk')
 const red = chalk.hex('#DC143C');
 
-const ban = (`
+const banner = (`
 ${chalk.hex('#FFFFFF')("                               ┬  ┬┬┌─┐┬ ┬")}
 ${chalk.hex('#8D8C8C')("                               └┐┌┘│├┤ │││")}
 ${chalk.hex('#DC143C')("                                └┘ ┴└─┘└┴┘")}
 `)
 
-console.log(ban)
+console.log(banner)
 
 async function heyy() {
     console.log((`${red(`Sent a view.`)}`))
-    const browser = await yo.launch();
+    const browser = await p.launch();
     const page = await browser.newPage(); //opens page
-    await page.goto("https://camo.githubusercontent.com/0611cf09a7206ca10457a2958e022e3bfe133ba2424edafdb068a30e20556bea/68747470733a2f2f677076632e6172747572696f2e6465762f693367617073")
+    await page.goto("Link")
     await browser.close();
 }
 
